5c4d9ea6e42c778b0f2deb256ef3a05270042008
xvrTamim <tamimazabir@gmail.com>
2022-04-26 22:56:51 +0600

    Add files via upload

---
+++ PHP/urapTable.php
@@ -0,0 +1,35 @@
+<?php
+
+//unused resources page
+$db = "iub_db";
+$host = "localhost";
+$db_user = 'root';
+$db_password = '';
+
+$link = mysqli_connect($host, $db_user, $db_password, $db);
+
+$json["error"] = false;
+$json["errmsg"] = "";
+$json["data"] = array();
+
+
+$sql = "";
+
+
+$res = mysqli_query($link, $sql);
+$numrows = mysqli_num_rows($res);
+if ($numrows > 0) {
+    $namelist = array();
+
+    while ($array = mysqli_fetch_assoc($res)) {
+        array_push($json["data"], $array);
+    }
+} else {
+    $json["error"] = true;
+    $json["errmsg"] = "No any data to show.";
+}
+
+mysqli_close($link);
+
+header('Content-Type: application/json');
+echo json_encode($json);
